119f68cf8cf8573ec19a0dd34be8ee225d3cf0cd
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-02-06 10:40:54 +0100

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="VSSPython",
+    version="1.0.0",
+    author="Julien Kauffmann",
+    author_email="julien.kauffmann@freelan.org",
+    description="A Python library used to interact with Microsoft Visual SourceSafe repositories.",
+    packages=['vss'],
+    classifiers=[],
+)
