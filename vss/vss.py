34df96b1181ab3213e39288397f64303c4bbe91b
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-02-06 13:15:13 +0100

    Added option support

--- vss/vss.py
+++ vss/vss.py
@@ -32,11 +32,60 @@ class VSS(object):
 
         return subprocess.check_output([self.ss_path] + argv, env=env)
 
+    def __to_options_list(self, options):
+        """
+        Convert options to their VSS format.
+        """
+
+        result = []
+
+        options_map = {
+            'format': {
+                'binary': '-B',
+                'text': '-B-',
+            },
+            'base_version_number': '-B{param}',
+            'base_version_date': '-B{param}',
+            'base_version_label': '-Bl{param}',
+        }
+
+        for option, value in options.items():
+
+            if not option in options_map:
+                raise ValueError('Invalid option name (%s)' % repr(option))
+
+            map_entry = options_map[option]
+
+            if isinstance(map_entry, dict):
+
+                if not value in map_entry:
+                    raise ValueError('Invalid option value for %s (%s)' % (repr(option), repr(value)))
+
+                result.append(map_entry[value])
+
+            elif isinstance(map_entry, str):
+
+                result.append(map_entry.replace('{param}', value))
+
+        return result
+
     def about(self):
         """
         Calls the VSS About command.
 
-        Returns the about information.
+        Returns the standard output.
         """
 
         return self.__execute(['About'])
+
+    def add(self, files, **options):
+        """
+        Calls the VSS Add command for the specified files.
+
+        Returns the standard output.
+        """
+
+        if not isinstance(files, list):
+            files = [str(files)]
+
+        return self.__execute(['Add'] + files + self.__to_options_list(options))
